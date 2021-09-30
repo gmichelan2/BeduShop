2fff37d5a45d243ae994bbb9da8ee7b5db4f4d15
Gaston <gaston.emiliano.michelan@gmail.com>
2021-09-29 21:49:26 -0300

    Arreglada barra de navegación

--- app/src/main/java/com/example/bedushop/LoggedActivity.kt
+++ app/src/main/java/com/example/bedushop/LoggedActivity.kt
@@ -2,12 +2,17 @@ package com.example.bedushop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.content.Intent
+import android.net.Uri
+
 
 /**
 Activity que maneja la aplicación y los intercambios de fragmentos, una vez que el usuario se ha logueado
@@ -51,6 +56,25 @@ class LoggedActivity : AppCompatActivity() {
             true
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater=menuInflater
+        inflater.inflate(R.menu.option_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.option_search -> Toast.makeText(this,"Opción no disponible", Toast.LENGTH_SHORT).show()
+            R.id.option_info->{
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://bedu.org/"))
+                startActivity(browserIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     //método para reemplazar los fragmentos de acuerdo al botón seleccionado en el navBottomMenu
