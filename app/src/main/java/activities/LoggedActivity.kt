0931a96a4ddfe8bcd2541aaee04a0ebe2f4ad560
Gaston <gaston.emiliano.michelan@gmail.com>
2021-10-21 14:52:09 -0300

    agregada la animación del bottomViewMenu

--- app/src/main/java/activities/LoggedActivity.kt
+++ app/src/main/java/activities/LoggedActivity.kt
@@ -1,5 +1,6 @@
 package activities
 
+import android.animation.AnimatorInflater
 import android.app.Person
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,16 +11,19 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
+import android.view.View
+import androidx.core.view.marginBottom
+import androidx.fragment.app.FragmentContainerView
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import clases.User
 import com.example.bedushop.*
-import com.google.gson.Gson
 import okhttp3.*
-import java.io.IOException
-import kotlin.random.Random
+
 
 
 /**
@@ -29,25 +33,72 @@ class LoggedActivity : AppCompatActivity() {
 
 
     private lateinit var bottomNav: BottomNavigationView
+    private var bottomShow=true
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var hostFragment: FragmentContainerView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loged)
 
+        hostFragment=findViewById<FragmentContainerView>(R.id.logged_nav_host_fragment)
 
         //seteo el fragmento por defecto el cual va a ser la lista con el recycler view
-       // replaceFragment(productListFragment)
+        // replaceFragment(productListFragment)
 
-        bottomNav=findViewById(R.id.bottom_navigation)
+        bottomNav = findViewById(R.id.bottom_navigation)
 
         //Busco el navHostFragment
-        val navHostFragment=supportFragmentManager.findFragmentById(R.id.logged_nav_host_fragment) as NavHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.logged_nav_host_fragment) as NavHostFragment
 
         //Seteo el botomNav pra qeu funcione con el navHostFragment
 
         bottomNav.setupWithNavController(navHostFragment.navController)//setea automaticamente las redirecciones en el bottomNav view
 
+        //seteo una animacion para que aparezca el bottom nav
+        lifecycleScope.launchWhenResumed {
+            navHostFragment.findNavController()
+                .addOnDestinationChangedListener { _, destination, _ ->
+                    Log.d("destination", destination.toString())
+                    when (destination.id) {
+                        R.id.nav_home -> {
+                           showBottomNav()
+                        }
+
+                        R.id.nav_cart -> {
+                            showBottomNav()
+                        }
+                        R.id.nav_profile -> {
+                            showBottomNav()
+                        }
+                        R.id.emptyCartFragment -> {
+                            showBottomNav()
+                        }
+                        else -> {
+                            AnimatorInflater.loadAnimator(this@LoggedActivity,R.animator.down).apply {
+                                setTarget(bottomNav)
+                                start() }
+                            bottomShow=false
+                            hostFragment.setPadding(0,0,0,0)
+
+                        }
+                    }
+                }
+        }
+    }
+
+
+    private fun showBottomNav(){
+        if (!bottomShow) {
+            AnimatorInflater.loadAnimator(this@LoggedActivity,R.animator.climb).apply {
+                setTarget(bottomNav)
+                start() }
+            bottomShow=true
+            hostFragment.setPadding(0,0,0,80)
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
